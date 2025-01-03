948da787d3a74a8dd6f43f9562ddc2e503fca76d
Mark Eric Fairweather <59223278+Fairweather13024@users.noreply.github.com>
2025-01-03 03:57:45 -0800

    Update Dashboard.tsx

--- meeting-minutes/src/pages/Dashboard.tsx
+++ meeting-minutes/src/pages/Dashboard.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
-
+const bgImg = require("../assets/images/mom.png");
 export default function Dashboard() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="text-center">
-          <img
-            src="/images/mom.png"
-            alt="Meeting"
-            className="mx-auto h-64 w-auto"
-          />
+          <img src={bgImg} alt="Meeting" className="mx-auto h-64 w-auto" />
           <div className="mt-8">
             <Link
               to="/upload"
