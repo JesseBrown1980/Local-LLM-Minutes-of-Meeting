ceed410749eb5f0b21a17f014e9f2f0daa14509e
Mark Eric Fairweather <59223278+Fairweather13024@users.noreply.github.com>
2025-01-03 01:29:31 -0800

    Create App.tsx

---
+++ meeting-minutes/src/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import TaskList from "./pages/TaskList";
+import TaskDetails from "./pages/TaskDetails";
+import UploadAudio from "./pages/UploadAudio";
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-100">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route
+                path="/"
+                element={<PrivateRoute element={<Dashboard />} />}
+              />
+              <Route
+                path="/upload"
+                element={<PrivateRoute element={<UploadAudio />} />}
+              />
+              <Route
+                path="/tasks"
+                element={<PrivateRoute element={<TaskList />} />}
+              />
+              <Route
+                path="/tasks/:taskId"
+                element={<PrivateRoute element={<TaskDetails />} />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
