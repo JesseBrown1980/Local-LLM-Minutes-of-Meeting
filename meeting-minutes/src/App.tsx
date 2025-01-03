1e62d6484545cc7644811d838863e2bb48e3bd79
Mark Eric Fairweather <59223278+Fairweather13024@users.noreply.github.com>
2025-01-03 03:57:33 -0800

    Update App.tsx

--- meeting-minutes/src/App.tsx
+++ meeting-minutes/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -11,34 +11,36 @@ import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <div className="min-h-screen bg-gray-100">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route
-                path="/"
-                element={<PrivateRoute element={<Dashboard />} />}
-              />
-              <Route
-                path="/upload"
-                element={<PrivateRoute element={<UploadAudio />} />}
-              />
-              <Route
-                path="/tasks"
-                element={<PrivateRoute element={<TaskList />} />}
-              />
-              <Route
-                path="/tasks/:taskId"
-                element={<PrivateRoute element={<TaskDetails />} />}
-              />
-            </Routes>
+    <BrowserRouter>
+      <AuthProvider>
+        <>
+          <div className="min-h-screen bg-gray-100">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                  path="/"
+                  element={<PrivateRoute element={<Dashboard />} />}
+                />
+                <Route
+                  path="/upload"
+                  element={<PrivateRoute element={<UploadAudio />} />}
+                />
+                <Route
+                  path="/tasks"
+                  element={<PrivateRoute element={<TaskList />} />}
+                />
+                <Route
+                  path="/tasks/:taskId"
+                  element={<PrivateRoute element={<TaskDetails />} />}
+                />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
+        </>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
